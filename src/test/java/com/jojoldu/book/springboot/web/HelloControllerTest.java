f3d7cbec28167b5f3bdb7019fa49800111466d6a
leeyongjoo <2yongjooo@gmail.com>
2020-05-08 23:22:44 +0900

    롬복 적용 테스트 완료

--- src/test/java/com/jojoldu/book/springboot/web/HelloControllerTest.java
+++ src/test/java/com/jojoldu/book/springboot/web/HelloControllerTest.java
@@ -8,8 +8,9 @@ import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+
+import static org.hamcrest.Matchers.is;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @RunWith(SpringRunner.class)    // 1)
 @WebMvcTest                     // 2)
@@ -25,5 +26,18 @@ public class HelloControllerTest {
         mvc.perform(get("/hello"))          // 5)
             .andExpect(status().isOk())         // 6)
             .andExpect(content().string(hello));    // 7)
+    }
+
+    @Test
+    public void return_helloDto() throws Exception {
+        String name = "hello";
+        int amount = 1000;
+
+        mvc.perform(get("/hello/dto")
+                .param("name", name) // 1)
+                .param("amount", String.valueOf(amount)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.name", is(name))) // 2)
+                .andExpect(jsonPath("$.amount", is(amount)));
     }
 }
